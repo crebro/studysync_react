bd535ba3695930b7a54f59313973cf7bf66ac23a
bimin koju <biminkoju@gmail.com>
2024-02-02 06:51:17 +0545

    idk what i was doing dump

--- src/pages/Home/Components/HeroSection.jsx
+++ src/pages/Home/Components/HeroSection.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import bgimage from 'assets/peoplestudying-home.jpg'
+import React from 'react';
+import bgimage from 'assets/peoplestudying-home.jpg';
 
 export function HeroSection() {
-    return (
-        <>
-            <div className='w-full h-[60vh] flex justify-center items-center' style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover' }}>
-                <div className='flex flex-col justify-center items-center backdrop-brightness-50 h-full w-full'>
-                    <div className='text-5xl font-bold text-center text-white'>
-                        StudySync; The <span className='text-blue-500 '>Collaborative </span> Learning Solution
-                    </div>
-                    <div className='text-2xl text-center text-white'>
-                        Because Team Work Makes the Dream Work
-                    </div>
-                </div>
-            </div>
-
-        </>
-    )
+	return (
+		<>
+			<div
+				className="w-full h-[60vh] flex justify-center items-center"
+				style={{
+					backgroundImage: `url(${bgimage})`,
+					backgroundSize: 'cover',
+				}}
+			>
+				<div className="flex flex-col justify-center items-center backdrop-brightness-50 h-full w-full">
+					<div className="text-5xl font-bold text-center text-white">
+						StudySync; The{' '}
+						<span className="text-blue-500 ">Collaborative </span>{' '}
+						Learning Solution
+					</div>
+					<div className="text-2xl text-center text-white">
+						Because Team Work Makes the Dream Work
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
